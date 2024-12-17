7b233abbe382ec9993d7590a2e226eb79ffea864
邹意 <741599086@qq.com>
2024-12-17 18:30:36 +0800

    更新参数

--- src/public/user_react/src/component/form/provinceCityArea/index.jsx
+++ src/public/user_react/src/component/form/provinceCityArea/index.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react';
 import { Cascader } from 'antd';
 import { useMount } from 'ahooks';
-import {regionApi} from '@/api/region';
+import { regionApi } from '@/api/region';
 
 /**
  * 省市区选择
- * @param {String} value 默认值
+ * @param {string} value 默认值
  * @param {fun} onChange 修改value事件
- * @param {String} valueType 返回的value值是要id还是要title
+ * @param {string} valueType 返回的value值是要id还是要title
  * @param {boolean } changeOnSelect 是否可直接选择某个省、或某个市，而不是必须要选择省》市》区
+ * @param {boolean } multiple 是否支持多选
+ * @param {string } expandTrigger 次级菜单的展开方式，可选 'click' 和 'hover'
  * @author zy <741599086@qq.com>
  * @link https://www.superadminx.com/
  */
-export default ({ value, onChange, valueType = 'title', changeOnSelect = false, ...props }) => {
+export default ({ value, onChange, valueType = 'title', changeOnSelect = false, multiple = false, expandTrigger = 'hover',...props }) => {
     const [list, setList] = useState([]);
     useMount(() => {
         regionApi.getListAll().then(res => {
@@ -28,6 +30,8 @@ export default ({ value, onChange, valueType = 'title', changeOnSelect = false, 
                     onChange(_value);
                 }}
                 changeOnSelect={changeOnSelect}
+                maxTagCount="responsive"
+                multiple={multiple}
                 options={list}
                 placeholder="请选择，支持搜索"
                 allowClear
@@ -38,7 +42,7 @@ export default ({ value, onChange, valueType = 'title', changeOnSelect = false, 
                     children: 'children'
                 }}
                 showSearch={true}
-                expandTrigger="hover"
+                expandTrigger={expandTrigger}
             />
         </>
     )
