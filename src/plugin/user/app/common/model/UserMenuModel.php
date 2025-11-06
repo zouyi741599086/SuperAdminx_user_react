a29a0e006bfeb5e7950aac40337fd84027efec41
邹意 <741599086@qq.com>
2025-11-06 16:39:38 +0800

    修复附件字段错误

--- src/plugin/user/app/common/model/UserMenuModel.php
+++ src/plugin/user/app/common/model/UserMenuModel.php
@@ -21,7 +21,7 @@ class UserMenuModel extends BaseModel
             'name'               => 'user_menu',
             'autoWriteTimestamp' => true,
             'type'               => [],
-            'file'               => [ // 包含附件的字段，''代表直接等于附件路劲，'array'代表数组中包含附件路劲，'editor'代表富文本中包含附件路劲
+            'fileField'          => [ // 包含附件的字段，''代表直接等于附件路劲，'array'代表数组中包含附件路劲，'editor'代表富文本中包含附件路劲
             ],
         ];
     }
